8958a4e147a6ebb3e309a2c1c23c5791dcca0dcd
Nivin Mathew S <nivinmathews@Nivins-MacBook-Air.local>
2025-10-03 16:25:22 +0530

    jg

---
+++ types/course.ts
@@ -0,0 +1,14 @@
+export type Course {
+  id: number;
+  title: string;
+  description?: string;
+  fullDescription?: string;
+  icon: string;
+  image: string;
+  tags: string[];
+  duration: string;
+  keyTopics?: string[];
+  courseStructure?: string[];
+  benefits?: string[];
+  careerOpportunities?: string[];
+}
